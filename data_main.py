907ac5f4207e0d3fee592b62bff1acd1c798a6fb
plain-chicken <578915093@qq.com>
2022-05-07 17:08:14 +0800

    基本接口

--- data_main.py
+++ data_main.py
@@ -1,3 +1,7 @@
+from asyncio.windows_events import NULL
+from pickle import TRUE
+import string
+from time import monotonic
 import cv2
 from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
 import os
@@ -5,8 +9,51 @@ from PIL import Image
  
 class DataMain(object):
 
-    videoPath = "C:\VScode\视频.mp4"  # 读取视频路径
-    imgPath = "C:\VScode\Result\\"  # 保存图片路径
+    test_mode = True
+
+    @property
+    def video_path(self):
+        return self.video_path
+    @video_path.setter
+    def video_path(self,value):
+        if isinstance(value,string):
+            self.video_path = value
+        else:
+            print("error")
+
+    @property
+    def imgset_path(self):
+        return self.imgset_path
+    @imgset_path.setter
+    def imgset_path(self,value):
+        if isinstance(value,string):
+            self.imgset_path = value
+        else:
+            print("error")
+
+    @property
+    def model(self):
+        return self.model
+    @model.setter
+    def model(self,value):
+        if isinstance(value,string):
+            self.model = value
+        else:
+            print("error")
+
+    @property
+    def taget_img_path(self):
+        return self.img_set_path
+    @taget_img_path.setter
+    def taget_img_path(self,value):
+        if isinstance(value,string):
+            self.img_set_path = value
+        else:
+            print("error")
+
+
+
+
 
     #图片压缩
     def image_processing(self):
@@ -20,12 +67,14 @@ class DataMain(object):
     #视频转图片
     def video_to_pic(self):
         cap = cv2.VideoCapture(self.videoPath)
+        if (cap==NULL):
+            print("Wrong video path")
+            return
         fps = cap.get(cv2.CAP_PROP_FPS)  # 获取帧率
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 获取宽度
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 获取高度
         suc = cap.isOpened()  # 是否成功打开
         frame_count = 0
-        print(int(fps))
         while suc:
             frame_count += 1
             suc, frame = cap.read()
@@ -39,6 +88,9 @@ class DataMain(object):
         print("视频转图片结束！")
 
 
+    def __init__(self):
+        pass
 
 if __name__ == '__main__':
-    image_processing()
+    data_class = DataMain()
+    data_class.image_processing()
