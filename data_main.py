2ecbb234ce67be6b1d5107616fa05e733d5bdba4
plain-chicken <578915093@qq.com>
2022-05-08 23:05:16 +0800

    可以从视频中导出图片并进行裁剪

--- data_main.py
+++ data_main.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 from pickle import TRUE
 import string
 from time import monotonic
@@ -6,91 +5,122 @@ import cv2
 from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
 import os
 from PIL import Image
- 
+from pathlib import Path
+ #交互端调用时，需修改 video_path , target_img_path 和 output_path，然后调用execute函数
+ #请确保output_path下没有1.jpg、2.jpg等文件
 class DataMain(object):
 
-    test_mode = True
+    def __init__(self):
+        pass
 
     @property
     def video_path(self):
-        return self.video_path
+        return self.__video_path
     @video_path.setter
     def video_path(self,value):
-        if isinstance(value,string):
-            self.video_path = value
+        if isinstance(value,str):
+            self.__video_path = value
         else:
             print("error")
 
     @property
+    def output_path(self):
+        return self.__output_path
+    @output_path.setter
+    def output_path(self,value):
+        if isinstance(value,str):
+            self.__output_path = value
+        else:
+            print("error")
+ 
+    @property
     def imgset_path(self):
-        return self.imgset_path
+        return self.__imgset_path
     @imgset_path.setter
     def imgset_path(self,value):
-        if isinstance(value,string):
-            self.imgset_path = value
+        if isinstance(value,str):
+            self.__imgset_path = value
         else:
             print("error")
 
     @property
     def model(self):
-        return self.model
+        return self.__model
     @model.setter
     def model(self,value):
-        if isinstance(value,string):
-            self.model = value
+        if isinstance(value,str):
+            self.__model = value
         else:
             print("error")
 
     @property
     def taget_img_path(self):
-        return self.img_set_path
+        return self.__taget_img_path
     @taget_img_path.setter
     def taget_img_path(self,value):
-        if isinstance(value,string):
-            self.img_set_path = value
+        if isinstance(value,str):
+            self.__taget_img_path = value
         else:
             print("error")
 
 
 
 
-
-    #图片压缩
-    def image_processing(self):
+    #图片压缩及保存
+    def img_compressing(self,image,order):
         #  待处理图片路径
-        img_path = Image.open('./images/1.png')
+        #img_path = Image.open('./images/1.png')
     	#  resize图片大小，入口参数为一个tuple，新的图片的大小
-        img_size = img_path.resize((520, 520))
+        compressed_image = image.resize((64, 128))
     	#  处理图片后存储路径，以及存储格式
-        img_size.save('./images_1/i.jpg', 'JPEG')
+        compressed_image.save(self.output_path+str(order)+".jpg", 'JPEG')
 
     #视频转图片
     def video_to_pic(self):
-        cap = cv2.VideoCapture(self.videoPath)
-        if (cap==NULL):
-            print("Wrong video path")
-            return
+        cap = cv2.VideoCapture(self.__video_path)
+        cv2.waitKey(0)
+        #print(self.__video_path)
+        # if (~suc):
+        #     print("Wrong video path")
+        #     return
         fps = cap.get(cv2.CAP_PROP_FPS)  # 获取帧率
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 获取宽度
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 获取高度
-        suc = cap.isOpened()  # 是否成功打开
+        len = min(width,height//2)
+        midx = width//2;
+        midy = height//2;
         frame_count = 0
-        while suc:
+        img_count = 0
+        flag =cap.isOpened()
+        while flag:
             frame_count += 1
-            suc, frame = cap.read()
-            if ~suc:
-                return
+            flag,frame = cap.read()
+            if not flag:
+                break
             if frame_count%int(fps+1) == 0 :
-                cv2.imwrite(self.imgPath + "%d.jpg" %frame_count, frame)
-                cv2.waitKey(1)
+                x0 = midx-len//2
+                x1 = midx+len//2
+                y0 = midy-len
+                y1 = midy+len
+                crop_img = frame[y0:y1,x0:x1] 
+                img_count+=1
+                #cv2.imwrite("C:\\VScode\\"+str(img_count)+".jpg", frame)
+                image = Image.fromarray(cv2.cvtColor(crop_img,cv2.COLOR_BGR2RGB))
+                self.img_compressing(image,img_count)
+                
+                
+            cv2.waitKey(1)    
             #cv2.imwrite(imgPath + str(frame_count).zfill(4), frame)     
         cap.release()
         print("视频转图片结束！")
 
+    def execute(self):
+        self.video_to_pic();
 
-    def __init__(self):
-        pass
+
 
 if __name__ == '__main__':
     data_class = DataMain()
-    data_class.image_processing()
+    data_class.video_path = "C:/VScode/Result.mp4"
+    data_class.output_path = "C:/Vscode/"
+    data_class.execute()
