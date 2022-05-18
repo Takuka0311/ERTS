7b5bcd467937274d960cb407a303ada776d3a824
alohakya <1523407063@qq.com>
2022-05-19 01:09:05 +0800

    UI美化

--- main.py
+++ main.py
@@ -5,10 +5,16 @@
 from PyQt5 import QtWidgets
 from interactive_main import MyWindow
 import sys
+from QSSLoader import QSSLoader
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     application = MyWindow()
+
+    style_file = 'style.qss'
+    style_sheet = QSSLoader.read_qss_file(style_file)
+    application.setStyleSheet(style_sheet)
+
     application.show()
     sys.exit(app.exec())
 
